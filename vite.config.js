6a5102c3c905d640ab9d4f72532b4d4dd009a2d9
Jacopo Carlini <jacopo.carlini@gmail.com>
2026-03-30 13:47:50 +0200

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // SOSTITUISCI CON IL NOME ESATTO DEL TUO REPOSITORY SU GITHUB (con le barre!)
+  base: '/librain/',
+
+  plugins: [
+    react(),
+    VitePWA({
+      // ... (il resto della tua configurazione PWA)
+    })
+  ]
 })
